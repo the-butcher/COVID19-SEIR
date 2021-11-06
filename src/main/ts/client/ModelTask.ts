ddf4f2b0900857a5c11cc781f9dafa9943c6a150
Fleischer <hannes.fleischer@vertigis.com>
2021-11-06 19:47:04 +0100

    misc

--- src/main/ts/client/ModelTask.ts
+++ src/main/ts/client/ModelTask.ts
@@ -83,11 +83,11 @@ export class ModelTask {
                     StorageUtil.getInstance().setSaveRequired(true);
                 });
 
-                // setTimeout(() => {
-                //     ChartAgeGroup.getInstance().exportToPng().then(() => {
-                //         ModelTask.commit('CONTACT', ControlsConstants.createWorkerInput());
-                //     });
-                // }, 3000);
+                setTimeout(() => {
+                    // ChartAgeGroup.getInstance().exportToPng().then(() => {
+                        ModelTask.commit('CONTACT', ControlsConstants.createWorkerInput());
+                    // });
+                }, 3000);
 
                 // show any contact updates
                 // const displayableModification = ControlsContact.getInstance().getModification();
